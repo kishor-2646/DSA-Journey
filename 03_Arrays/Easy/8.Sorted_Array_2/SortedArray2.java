490b06326f6833e88fe4e1ae0bab8dfbbe62893f
kishor-2646 <kishorekumar20002646@gmail.com>
2026-04-03 22:44:57 +0530

    Added 8.Sorted Array 2

---
+++ 03_Arrays/Easy/8.Sorted_Array_2/SortedArray2.java
@@ -0,0 +1,29 @@
+class Solution {
+    // Function to check if the array is sorted
+    public boolean isSorted(int[] nums) {
+       int n = nums.length;
+       int count = 0;
+
+       for(int i = 0; i < n-1; i++)
+       {
+        if(nums[i] > nums[i + 1])
+        count++;
+       }
+
+       if(count > 1)return false;
+
+       if(count == 1 && nums[n-1] > nums[0])return false;
+
+       return true;
+    }
+}
+
+public class SortedArray2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        Solution obj = new Solution();
+        
+        // Output result
+        System.out.println(obj.isSorted(arr) ? "True" : "False");
+    }
+}
