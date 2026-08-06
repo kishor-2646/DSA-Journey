0e2e485846113515a4cbd13c3cef15a6657dc21d
kishor-2646 <kishorekumar20002646@gmail.com>
2026-08-06 19:53:04 +0530

    Add 0165 - Compare Version Numbers

--- LeetCode/0165_Compare_Version_Numbers/Solution.java
+++ LeetCode/0165_Compare_Version_Numbers/Solution.java
@@ -1,4 +1,52 @@
 class Solution {
+   // optimal approach s(n) = O(1) , t(n) = O(n + m)
+
+    public int compareVersion(String version1, String version2) {
+          int n = version1.length();
+          int m = version2.length();
+
+          int i = 0, j = 0;
+
+          while(i < n || j < m)
+          {
+            int d1 = 0;
+            int d2 = 0;
+
+            while(i < n && version1.charAt(i) != '.')
+            {
+                d1 = d1 * 10 + version1.charAt(i) - '0';
+                i++;
+            }
+
+            while(j < m && version2.charAt(j) != '.')
+            {
+                d2 = d2 * 10 + version2.charAt(j) - '0';
+                j++;
+            }
+
+            if(d1 > d2)
+                return 1;
+
+            if(d2 > d1)
+                return -1;
+
+
+            i++;
+            j++;
+          }  
+
+            return 0;            
+            
+    }
+    
+
+
+
+
+
+   /* 
+            Better Approach s(n) = O(n + m) , t(n) = O(n + m)
+
     public int compareVersion(String version1, String version2) {
         String[] v1 = version1.split("\\.");
         String[] v2 = version2.split("\\.");
@@ -18,5 +66,5 @@ class Solution {
         }
 
         return 0;
-    }
+    } */
 }
