dbd92a05d2701717ef9e267cf2ea387f43527934
kishor-2646 <kishorekumar20002646@gmail.com>
2026-08-07 07:56:07 +0530

    Add 0125 - Valid Palindrome

--- LeetCode/0125_Valid_Palindrome/Solution.java
+++ LeetCode/0125_Valid_Palindrome/Solution.java
@@ -5,14 +5,14 @@ class Solution {
 
         while(left < right)
         {
-            while(left < right && Character.isLetterOrDigit(s.charAt(left)))
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left)))
                 left++;
 
             
-            while(left < right && Character.isLetterOrDigit(s.charAt(right)))
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right)))
                 right--;
 
-            if(Character.toLowerCase(s.charAt(left) != Character.toLowerCase(s.charAt(right))))
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right)))
                 return false;
 
             left++;
