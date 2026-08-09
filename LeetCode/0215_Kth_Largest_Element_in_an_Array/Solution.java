499e2c50ffcdc86179e5a5cccb39a1848d28acd8
kishor-2646 <kishorekumar20002646@gmail.com>
2026-08-09 11:08:54 +0530

    Add 0215 - Kth Largest Element in an Array

--- LeetCode/0215_Kth_Largest_Element_in_an_Array/Solution.java
+++ LeetCode/0215_Kth_Largest_Element_in_an_Array/Solution.java
@@ -1,5 +1,60 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
+
+    // using Quick select approach T(n) = 0(n) , O(n * n)  S(n) = O(1)
+     public int findKthLargest(int[] nums, int k) {
+                int n = nums.length;
+
+                int low = 0, high = n - 1;
+                int target = n - k;
+
+                while(low <= high)
+                {
+                    int pivot = partition(nums, low, high);
+
+                    if(pivot == target)
+                        return nums[pivot];
+                    else if(pivot < target)
+                        low = pivot + 1;
+                    else
+                        high = pivot - 1;
+                }
+                return -1;    
+     }
+
+     public int partition(int[] nums, int left, int right)
+     {
+        int i = left;
+        int pivot = nums[right];
+
+        for(int j = left; j < right; j++)
+        {
+            if(nums[j] < pivot)
+            {
+                swap(nums, j, i);
+                i++;
+            }
+        }
+
+        swap(nums, i, right);
+        return i;
+     }
+
+     public void swap(int[] nums,int n, int m)
+     {
+        int temp = nums[n];
+        nums[n] = nums[m];
+        nums[m] = temp;
+     }
+  
+  
+  
+  
+  
+  
+   /*  
+            Using MinHeap version T(n) = O(n log K) , S(n) = O(k)
+   
+   public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
         for(int n: nums)
@@ -13,5 +68,5 @@ class Solution {
         }
 
         return minHeap.peek();
-    }
+    } */
 }
