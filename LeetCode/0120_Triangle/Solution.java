5ab89cd7d176e81fa6ddaec4a091e31bccfda230
kishor-2646 <kishorekumar20002646@gmail.com>
2026-08-11 21:37:41 +0530

    Add 0120 - Triangle

--- LeetCode/0120_Triangle/Solution.java
+++ LeetCode/0120_Triangle/Solution.java
@@ -1,5 +1,32 @@
 class Solution {
-         public int minimumTotal(List<List<Integer>> triangle) {
+
+
+public int minimumTotal(List<List<Integer>> triangle) {
+
+                int n = triangle.size();
+
+                int[] dp = new int[n];
+
+                for(int j = 0; j < n; j++)
+                {
+                    dp[j] = triangle.get(n - 1).get(j);
+                }
+
+                for(int i = n - 2; i >= 0; i--)
+                {
+                    for(int j = 0; j <= i; j++)
+                    {
+                        dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
+                    }
+                }
+
+                return dp[0];
+         
+}
+
+         /*
+         
+          public int minimumTotal(List<List<Integer>> triangle) {
 
                 int n = triangle.size();
 
@@ -19,10 +46,7 @@ class Solution {
                 }
 
                 return dp[0][0];
-         
-         
-         
-         }
+         } */
 
 }
 
