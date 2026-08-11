ec49d3eb42e3251198e9cab8f4e762118362f3ff
kishor-2646 <kishorekumar20002646@gmail.com>
2026-08-11 20:33:25 +0530

    Add 0120 - Triangle

--- LeetCode/0120_Triangle/Solution.java
+++ LeetCode/0120_Triangle/Solution.java
@@ -1,9 +1,44 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        return solve(triangle, 0, 0);
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+
+        for(int[] num: dp)
+        {
+            Arrays.fill(num,Integer.MIN_VALUE);
+        }
+        
+        return solve(triangle, 0, 0, dp);
     }
 
-    public int solve(List<List<Integer>> triangle, int row, int col)
+
+
+    private int solve(
+            List<List<Integer>> triangle,
+            int row,
+            int col,
+            int[][] dp)    {
+        if(row == triangle.size() - 1)
+            return triangle.get(row).get(col);
+        
+        if(dp[row][col] != Integer.MIN_VALUE)
+            return triangle.get(row).get(col);
+
+        int left = solve(triangle, row + 1, col, dp);
+        int right = solve(triangle, row + 1, col + 1, dp);
+
+        dp[row][col] = triangle.get(row).get(col) + Math.min(left, right);
+
+        return dp[row][col];
+    }
+
+
+
+
+  /*  
+            Brute force approach T(N) = O(2^n)
+  
+   public int solve(List<List<Integer>> triangle, int row, int col)
     {
         if(row == triangle.size() - 1)
             return triangle.get(row).get(col);
@@ -12,5 +47,5 @@ class Solution {
         int right = solve(triangle, row+ 1, col+ 1);
 
         return triangle.get(row).get(col) + Math.min(left,right);
-    }
+    } */
 }
