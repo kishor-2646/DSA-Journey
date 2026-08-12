177351138264b9d5d1994ed43f8fa507ddfe1ade
kishor-2646 <kishorekumar20002646@gmail.com>
2026-08-12 09:21:06 +0530

    Add 0028 - Find the Index of the First Occurrence in a String

--- LeetCode/0028_Find_the_Index_of_the_First_Occurrence_in_a_String/Solution.java
+++ LeetCode/0028_Find_the_Index_of_the_First_Occurrence_in_a_String/Solution.java
@@ -1,5 +1,92 @@
 class Solution {
+   
+   // KMP - LPS[] array Approach T(N) = O(n + m)  S(N) = O(M)
     public int strStr(String haystack, String needle) {
+            int n = haystack.length();
+            int m = needle.length();
+
+            int i = 0, j = 0;
+
+            int[] lps = new int[m];
+
+            computeLPS(lps,needle);
+
+            while(i < n)
+            {
+                if(haystack.charAt(i) == needle.charAt(j))
+                {
+                    i++;
+                    j++;
+
+                    if(j == m)
+                    {
+                        return i - j;
+                    }
+
+                }
+                else
+                {
+                    if(j != 0)
+                    {
+                        j = lps[j - 1];
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+            }
+   
+   return -1;
+   
+   
+   }
+
+   public void computeLPS(int[] lps, String p)
+   {
+    int n = p.length();
+    int i = 0, len = 0;
+
+    lps[i] = 0;
+    i = 1;
+
+    while(i < n)
+    {
+        if(p.charAt(i) == p.charAt(len))
+        {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else
+        {
+            if(len != 0)
+            {
+                len = lps[len - 1];
+            }
+            else
+            {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   /*  
+      Brute force T(N) = O(n × m), S(N) = O(1)
+
+   public int strStr(String haystack, String needle) {
         int n = haystack.length();
         int m = needle.length();
 
@@ -20,5 +107,5 @@ class Solution {
         }
 
         return -1;
-    }
+    } */
 }
